1b500bf8128b9ae41fc9c217df262597651599fc
Diablohu <diablohudream@gmail.com>
2017-05-03 15:34:28 +0800

    fixes

--- src/helpers.js
+++ src/helpers.js
@@ -4,6 +4,6 @@ const {
 } = require('./variables')
 
 module.exports = {
-    getSpeed = (speedId = 0) => speed[parseInt(speedId)] || '',
-    getRange = (rangeId = 0) => range[parseInt(rangeId)] || ''
+    getSpeed: (speedId = 0) => speed[parseInt(speedId)] || '',
+    getRange: (rangeId = 0) => range[parseInt(rangeId)] || ''
 }
