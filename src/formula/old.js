62389d2da9c9f86260569b7a3bfbe53fa9d02f4e
Diablohu <diablohudream@gmail.com>
2017-05-08 22:17:25 +0800

    added default speed rule

--- src/formula/old.js
+++ src/formula/old.js
@@ -1404,7 +1404,7 @@
             '34': 0,
             '87': 0
         }
-        let rule = ship._speedRule
+        let rule = ship._speedRule || 'low-2'
         let multiper = 0
 
         equipments_by_slot.forEach(function (equipment) {
