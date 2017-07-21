2c55b39cc4e8da6cabcfbfbb67181b07419b091e
Diablohu <diablohudream@gmail.com>
2017-07-21 17:29:56 +0800

    check.ship

--- src/check/ship.js
+++ src/check/ship.js
@@ -1,21 +1,100 @@
+const getShip = require('../get/ship')
+
 /**
- * 
+ * 检查舰娘是否满足给定条件
  * 
  * @param {(number|Ship)} ship 要判断的舰娘
- * @param {any} [condition={}] 
- * @param {(number|number[])} [condition.isType] 判断舰娘是否属于给定类型
- * @param {boolean} [condition.isBattleship]
- * @param {boolean} [condition.isBB]
- * @param {boolean} [condition.isCarrier]
- * @param {boolean} [condition.isCV]
- * @param {boolean} [condition.isSubmarine]
- * @param {boolean} [condition.isSS]
- * @param {(number|number[])} [condition.isID] 判断舰娘ID是否精确匹配
- * @param {(string|string[])} [condition.isName] 判断舰娘名是否精确匹配
- * @param {number} [condition.slotAtLeast] 判断舰娘的可配置栏位至少有 number 个
- * @param {number} [condition.slotIs] 判断舰娘的可配置栏位精确有 number 个
- * @param {number} [condition.slotAtMost] 判断舰娘的可配置栏位最多有 number 个
+ * @param {any} [conditions={}] 条件
+ * @param {(number|number[])} [conditions.isID] 判断舰娘ID是否精确匹配或匹配其中一项
+ * @param {(string|string[])} [conditions.isName] 判断舰娘名是否精确匹配或匹配其中一项
+ * @param {(number|number[])} [conditions.isType] 判断舰娘是否属于给定类型或匹配其中一项
+ * @param {boolean} [conditions.isBattleship]
+ * @param {boolean} [conditions.isBB]
+ * @param {boolean} [conditions.isCarrier]
+ * @param {boolean} [conditions.isCV]
+ * @param {boolean} [conditions.isSubmarine]
+ * @param {boolean} [conditions.isSS]
+ * @param {number|[min,max]} [conditions.isSlot] 判断舰娘的可配置栏位精确有 number 个，或 min ~ max 个
+ * @param {number} [conditions.isSlotMin] 判断舰娘的可配置栏位至少有 number 个
+ * @param {number} [conditions.isSlotMax] 判断舰娘的可配置栏位最多有 number 个
  */
-module.exports = (ship, condition = {}) => {
-    
+module.exports = (ship, conditions = {}) => {
+    ship = getShip(ship)
+    if (typeof ship === 'undefined') return false
+
+    for (let key in conditions) {
+        if (!checkCondition[key.toLowerCase()](ship, conditions[key]))
+            return false
+    }
+
+    return true
+}
+
+const ArrayOrItem = (arg, func) => {
+    if (Array.isArray(arg))
+        return arg.some(func)
+    return func(arg)
+}
+
+const checkCondition = {
+    // isID
+    isid: (ship, id) => ArrayOrItem(id, id => {
+        if (isNaN(id)) return false
+        return parseInt(id) === ship.id
+    }),
+
+    // isName
+    isname: (ship, name) => ArrayOrItem(name, name => {
+        for (let key in ship.name) {
+            if (key === 'suffix') continue
+            if (ship.name[key].toLowerCase() === name) return true
+        }
+        return false
+    }),
+
+    // isType
+    istype: (ship, type) => ArrayOrItem(type, type => {
+        if (isNaN(type)) return false
+        return parseInt(type) === ship.type
+    }),
+    isbattleship: function (ship, isTrue) {
+        return (this.istype(ship, [8, 6, 20, 7, 18]) === isTrue)
+    },
+    isbb: function (ship, isTrue) {
+        return this.isbattleship(ship, isTrue)
+    },
+    iscarrier: function (ship, isTrue) {
+        return (this.istype(ship, [11, 10, 9, 30, 32]) === isTrue)
+    },
+    iscv: function (ship, isTrue) {
+        return this.iscarrier(ship, isTrue)
+    },
+    issubmarine: function (ship, isTrue) {
+        return (this.istype(ship, [14, 13]) === isTrue)
+    },
+    isss: function (ship, isTrue) {
+        return this.issubmarine(ship, isTrue)
+    },
+
+    // isSlot
+    isslot: (ship, num) => {
+        if (!Array.isArray(ship.slot)) return false
+        if (Array.isArray(num)) {
+            if (isNaN(num[0]) && !isNaN(num[1]))
+                return ship.slot.length <= parseInt(num[1])
+            else if (!isNaN(num[0]) && isNaN(num[1]))
+                return ship.slot.length >= parseInt(num[0])
+            else if (!isNaN(num[0]) && !isNaN(num[1]))
+                return ship.slot.length >= parseInt(num[0]) && ship.slot.length <= parseInt(num[1])
+            else
+                return false
+        } else
+            return !isNaN(num) && parseInt(num) === ship.slot.length
+    },
+    isslotmin: function (ship, min) {
+        return this.isslot(ship, [min, undefined])
+    },
+    isslotmax: function (ship, max) {
+        return this.isslot(ship, [undefined, max])
+    },
 }
--- test/check.test.js
+++ test/check.test.js
@@ -10,13 +10,78 @@ beforeAll(() => new Promise(async resolve => {
     require('../src/register.js')({ db })
 }))
 
-const check = require ('../src/check')
+const check = require('../src/check')
 
 
 describe('Checking functions/utilities', () => {
     describe('Checking for ship...', () => {
-        it(`should db.entities[1] be instanceof Entity`, function () {
-            expect(true).toBe(db.entities[1] instanceof require('../src/class/entity.js'));
+        it(`should condition isID be working`, function () {
+            expect(check.ship(1, { isID: 1 })).toBe(true);
+            expect(check.ship(1, { isID: 2 })).toBe(false);
+            expect(check.ship(1, { isID: [1, 2] })).toBe(true);
+            expect(check.ship(db.ships[1], { isID: 1 })).toBe(true);
+            expect(check.ship(db.ships[1], { isID: 2 })).toBe(false);
+            expect(check.ship(db.ships[1], { isID: [1, 2] })).toBe(true);
         });
+        it(`should condition isName be working`, function () {
+            expect(check.ship(1, { isName: 'mutsuki' })).toBe(true);
+            expect(check.ship(1, { isName: '睦月' })).toBe(true);
+            expect(check.ship(1, { isName: 'むつき' })).toBe(true);
+            expect(check.ship(1, { isName: 'satsuki' })).toBe(false);
+            expect(check.ship(1, { isName: ['mutsuki', 'satsuki'] })).toBe(true);
+            expect(check.ship(db.ships[1], { isName: 'mutsuki' })).toBe(true);
+            expect(check.ship(db.ships[1], { isName: '睦月' })).toBe(true);
+            expect(check.ship(db.ships[1], { isName: 'むつき' })).toBe(true);
+            expect(check.ship(db.ships[1], { isName: 'satsuki' })).toBe(false);
+            expect(check.ship(db.ships[1], { isName: ['mutsuki', 'satsuki'] })).toBe(true);
+        });
+        it(`should condition isType (and other similar method) be working`, function () {
+            expect(check.ship(1, { isType: 1 })).toBe(true);
+            expect(check.ship(1, { isType: 2 })).toBe(false);
+            expect(check.ship(1, { isType: [1, 2] })).toBe(true);
+            expect(check.ship(db.ships[1], { isType: 1 })).toBe(true);
+            expect(check.ship(db.ships[1], { isType: 2 })).toBe(false);
+            expect(check.ship(db.ships[1], { isType: [1, 2] })).toBe(true);
+
+            expect(check.ship(1, { isBattleship: true })).toBe(false);
+            expect(check.ship(1, { isBattleship: false })).toBe(true);
+            expect(check.ship(1, { isBB: true })).toBe(false);
+            expect(check.ship(1, { isBB: false })).toBe(true);
+            expect(check.ship(86, { isBattleship: true })).toBe(true);
+            expect(check.ship(86, { isBattleship: false })).toBe(false);
+            expect(check.ship(86, { isBB: true })).toBe(true);
+            expect(check.ship(86, { isBB: false })).toBe(false);
+
+            expect(check.ship(1, { isCarrier: true })).toBe(false);
+            expect(check.ship(1, { isCarrier: false })).toBe(true);
+            expect(check.ship(1, { isCV: true })).toBe(false);
+            expect(check.ship(1, { isCV: false })).toBe(true);
+            expect(check.ship(404, { isCarrier: true })).toBe(true);
+            expect(check.ship(404, { isCarrier: false })).toBe(false);
+            expect(check.ship(404, { isCV: true })).toBe(true);
+            expect(check.ship(404, { isCV: false })).toBe(false);
+
+            expect(check.ship(1, { isSubmarine: true })).toBe(false);
+            expect(check.ship(1, { isSubmarine: false })).toBe(true);
+            expect(check.ship(1, { isSS: true })).toBe(false);
+            expect(check.ship(1, { isSS: false })).toBe(true);
+            expect(check.ship(163, { isSubmarine: true })).toBe(true);
+            expect(check.ship(163, { isSubmarine: false })).toBe(false);
+            expect(check.ship(163, { isSS: true })).toBe(true);
+            expect(check.ship(163, { isSS: false })).toBe(false);
+        });
+        it(`should condition isSlot (and other similar method) be working`, function () {
+            expect(check.ship(1, { isSlot: 2 })).toBe(true);
+            expect(check.ship(1, { isSlot: 1 })).toBe(false);
+            expect(check.ship(1, { isSlot: [2, 2] })).toBe(true);
+            expect(check.ship(1, { isSlot: [1, 3] })).toBe(true);
+            expect(check.ship(1, { isSlot: [3, 4] })).toBe(false);
+            expect(check.ship(1, { isSlotMin: 1 })).toBe(true);
+            expect(check.ship(1, { isSlotMin: 2 })).toBe(true);
+            expect(check.ship(1, { isSlotMin: 3 })).toBe(false);
+            expect(check.ship(1, { isSlotMax: 1 })).toBe(false);
+            expect(check.ship(1, { isSlotMax: 2 })).toBe(true);
+            expect(check.ship(1, { isSlotMax: 3 })).toBe(true);
+        })
     });
 })
