8e45024b12574f9c08fea1ea59e8d95e94957edd
Diablohu <diablohudream@gmail.com>
2017-07-24 19:43:25 +0800

    remove statics from main exports

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 module.exports = {
+    // Variables
     vars: require('./src/variables'),
+
+    // Functions
     get: require('./src/get'),
     data: require('./src/data'),
     check: require('./src/check'),
     calculate: require('./src/calculate'),
 
+    // Statics
+    // types: require('./src/types'),
+
     // Classes
-    Base: require('./src/class/base'),
-    Ship: require('./src/class/ship'),
-    Equipment: require('./src/class/equipment'),
-    Entity: require('./src/class/entity'),
-    Consumable: require('./src/class/consumable'),
+    // Base: require('./src/class/base'),
+    // Ship: require('./src/class/ship'),
+    // Equipment: require('./src/class/equipment'),
+    // Entity: require('./src/class/entity'),
+    // Consumable: require('./src/class/consumable'),
 
     // Formulas
     // formula: require('./src/formula/'),
 
     // Helper functions
-    register: require('./src/register'),
-    parseRaw: require('./src/parse-raw')
+    register: require('./src/register')
+    // parseRaw: require('./src/parse-raw')
 }
