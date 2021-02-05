ac4babe9b60d518621a8eadfa2310d56e94083b2
Diablohu <Diablohu@users.noreply.github.com>
2021-02-06 06:00:24 +0800

    update code from kckit v1

--- src/calculate/bonus.js
+++ src/calculate/bonus.js
@@ -1,7 +1,7 @@
-const getShipAndEquipments = require('../get/ship-and-equipments')
-const checkShip = require('../check/ship')
-const checkEquipments = require('../check/equipments')
-const bonus = require('../data/bonus')
+const getShipAndEquipments = require('../get/ship-and-equipments');
+const checkShip = require('../check/ship');
+const checkEquipments = require('../check/equipments');
+const bonus = require('../data/bonus');
 
 /**
  * Calculate stat bonus for specified ship with equipment(s)
@@ -11,121 +11,151 @@ const calculateBonus = (
     equipments = [],
     equipmentStars = [],
     equipmentRanks = [],
-    stat,
+    stat
 ) => {
     if (typeof equipmentStars === 'string')
-        return calculateBonus(ship, equipments, undefined, undefined, equipmentStars);
+        return calculateBonus(
+            ship,
+            equipments,
+            undefined,
+            undefined,
+            equipmentStars
+        );
     if (typeof equipmentRanks === 'string')
-        return calculateBonus(ship, equipments, equipmentStars, undefined, equipmentRanks);
+        return calculateBonus(
+            ship,
+            equipments,
+            equipmentStars,
+            undefined,
+            equipmentRanks
+        );
 
     ({
-        ship, equipments, equipmentStars, equipmentRanks
-    } = getShipAndEquipments(ship, equipments, equipmentStars, equipmentRanks))
+        ship,
+        equipments,
+        equipmentStars,
+        equipmentRanks
+    } = getShipAndEquipments(ship, equipments, equipmentStars, equipmentRanks));
 
-    const result = {}
-    const conditions = bonus.filter(bonus => checkShip(ship, bonus.ship))
+    const result = {};
+    const conditions = bonus.filter(bonus => checkShip(ship, bonus.ship));
     const addResult = (bonus = {}) => {
-        for (let stat in bonus) {
-            if (typeof result[stat] === 'undefined')
-                result[stat] = bonus[stat]
+        for (const stat in bonus) {
+            if (typeof result[stat] === 'undefined') result[stat] = bonus[stat];
             else if (typeof result[stat] === 'number')
-                result[stat] += bonus[stat]
-            else
-                result[stat] = bonus[stat]
+                result[stat] += bonus[stat];
+            else result[stat] = bonus[stat];
         }
-    }
+    };
 
     // 条件：单一装备
     conditions
-        .filter(bonus => (
-            typeof bonus.equipment === 'number' &&
-            equipments.some(equipment => (
-                equipment && equipment.id &&
-                equipment.id == bonus.equipment
-            ))
-        ))
+        .filter(
+            bonus =>
+                typeof bonus.equipment === 'number' &&
+                equipments.some(
+                    equipment =>
+                        equipment &&
+                        equipment.id &&
+                        equipment.id == bonus.equipment
+                )
+        )
         .forEach(bonus => {
-            let thisBonus = {}
+            let thisBonus = {};
 
             // 根据改修星级
             // 目前的数据结构下，改修星级条件与其他条件不能共存
             // TODO: 改修星级条件与数量条件可并存
             if (typeof bonus.bonusImprove === 'object') {
                 // 从大到小排序可能的改修星级
-                const starsDesc = Object
-                    .keys(bonus.bonusImprove)
-                    .sort((a, b) => parseInt(b) - parseInt(a))
+                const starsDesc = Object.keys(bonus.bonusImprove).sort(
+                    (a, b) => parseInt(b) - parseInt(a)
+                );
                 // 每个装备的收益单独计算
                 equipments.forEach((equipment, index) => {
-                    if (equipment && equipment.id &&
+                    if (
+                        equipment &&
+                        equipment.id &&
                         equipment.id == bonus.equipment
                     ) {
                         starsDesc.some(star => {
                             if (equipmentStars[index] >= star) {
-                                for (let stat in bonus.bonusImprove[star]) {
+                                for (const stat in bonus.bonusImprove[star]) {
                                     if (typeof thisBonus[stat] === 'undefined')
-                                        thisBonus[stat] = bonus.bonusImprove[star][stat]
-                                    else if (typeof thisBonus[stat] === 'number')
-                                        thisBonus[stat] += bonus.bonusImprove[star][stat]
+                                        thisBonus[stat] =
+                                            bonus.bonusImprove[star][stat];
+                                    else if (
+                                        typeof thisBonus[stat] === 'number'
+                                    )
+                                        thisBonus[stat] +=
+                                            bonus.bonusImprove[star][stat];
                                     else
-                                        thisBonus[stat] = bonus.bonusImprove[star][stat]
+                                        thisBonus[stat] =
+                                            bonus.bonusImprove[star][stat];
                                 }
-                                return true
+                                return true;
                             }
-                            return false
-                        })
+                            return false;
+                        });
                     }
-                })
+                });
             } else {
-                let thisCount = 0
+                let thisCount = 0;
 
                 // 统计目标装备的数量
                 equipments.forEach(equipment => {
-                    if (equipment && equipment.id &&
+                    if (
+                        equipment &&
+                        equipment.id &&
                         equipment.id == bonus.equipment
                     ) {
-                        thisCount++
+                        thisCount++;
                     }
-                })
+                });
 
                 if (typeof bonus.bonusCount === 'object') {
-                    Object
-                        .keys(bonus.bonusCount)
+                    Object.keys(bonus.bonusCount)
                         .sort((a, b) => parseInt(b) - parseInt(a))
                         .some(count => {
                             if (thisCount >= count) {
-                                thisBonus = { ...bonus.bonusCount[count] }
-                                return true
+                                thisBonus = { ...bonus.bonusCount[count] };
+                                return true;
                             }
-                            return false
-                        })
+                            return false;
+                        });
                 } else if (typeof bonus.bonus === 'object') {
-                    thisBonus = { ...bonus.bonus }
+                    thisBonus = { ...bonus.bonus };
                     // add for count
-                    for (let stat in thisBonus)
-                        thisBonus[stat] = typeof thisBonus[stat] === 'number'
-                            ? thisBonus[stat] * thisCount
-                            : parseInt(thisBonus[stat])
+                    for (const stat in thisBonus)
+                        thisBonus[stat] =
+                            typeof thisBonus[stat] === 'number'
+                                ? thisBonus[stat] * thisCount
+                                : parseInt(thisBonus[stat]);
                 }
             }
 
-            addResult(thisBonus)
-        })
+            addResult(thisBonus);
+        });
 
     // 条件：套装
     conditions
-        .filter(bonus => (
-            typeof bonus.equipments === 'object' &&
-            checkEquipments(equipments, equipmentStars, equipmentRanks, bonus.equipments)
-        ))
+        .filter(
+            bonus =>
+                typeof bonus.equipments === 'object' &&
+                checkEquipments(
+                    equipments,
+                    equipmentStars,
+                    equipmentRanks,
+                    bonus.equipments
+                )
+        )
         .forEach(bonus => {
-            addResult(bonus.bonus)
-        })
+            addResult(bonus.bonus);
+        });
 
-    if (typeof stat === 'string')
-        return result[stat] || 0
+    if (typeof stat === 'string') return result[stat] || 0;
 
-    return result
-}
+    return result;
+};
 
-module.exports = calculateBonus
+module.exports = calculateBonus;
